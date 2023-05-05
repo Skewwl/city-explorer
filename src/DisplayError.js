304956f531b6101eebae5a7d5f37efbfe27e747b
rhettb253 <rhettbeardemphl@hotmail.com>
2023-05-04 19:30:49 -0700

    getting ready to display error

---
+++ src/DisplayError.js
@@ -0,0 +1,13 @@
+// import Row from 'react-bootstrap/Row';
+// import Card from 'react-bootstrap/Card';
+// import Col from 'react-bootstrap/Col';
+
+function DisplayError(props) {
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default DisplayError;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,21 +5,29 @@ import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import CityData from './CityData';
+import DisplayError from './DisplayError';
 
 function SearchBar() {
 
     let [displayInfo, setDisplayInfo] = useState(false);
     let [city, setCity] = useState();
     let [recievedData, setRecievedData] = useState({});
+    let [err, setErr] = useState(null);
 
 
     let handleExplore = async (e) => {
         e.preventDefault();
-        let infoUrl = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${city}&format=json`;
-        const response = await axios.get(infoUrl);
-        setDisplayInfo(true);
-        setRecievedData(response.data);
-        console.log(response.data);
+        try {
+            let infoUrl = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${city}&format=json`;
+            const responsePromise = await axios.get(infoUrl);
+            responsePromise && setDisplayInfo(true);
+            setRecievedData(responsePromise.data);
+            setErr(null);
+        }
+        catch (error) {
+            console.error(error);
+            setErr(error.message);
+        }
     };
 
     return (
@@ -39,6 +47,7 @@ function SearchBar() {
                 <Col></Col>
             </Row>
             <CityData className="city-data" displayInfo={displayInfo} recievedData={recievedData} />
+            <DisplayError error={err}/>
         </>
     );
 }
